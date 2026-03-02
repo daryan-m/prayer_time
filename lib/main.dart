a4e720d13cc6b13801acc22d7152b0fb80fffc93
islandwaqwaq <islandwaqwaq@gmail.com>
2026-03-02 20:32:50 +0000

    v1.0.4

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 import 'package:timezone/data/latest.dart' as tz;
-import 'package:timezone/timezone.dart'
-    as tz; // ئەم دووانە وەک یەک ناویان لێنراوە، کێشە نییە
+import 'package:timezone/timezone.dart' as tz; // ئەم دووانە وەک یەک ناویان لێنراوە، کێشە نییە
 import 'screens/home_screen.dart';
 
 // ئەم دێڕە پێویستە لە دەرەوەی main بێت
